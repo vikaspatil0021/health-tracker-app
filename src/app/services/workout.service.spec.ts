895f7a202aa2d39efe8548b35d5ac1dfced1303a
Vikas Patil <vikaspatil0021o+github@gmail.com>
2025-02-01 11:11:57 +0530

    workoutservice: added getAllWorkouts and filterWorkouts method, wrote test cases with 100% coverage

--- src/app/services/workout.service.spec.ts
+++ src/app/services/workout.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
-import { WorkoutService } from './workout.service';
+import { workoutData } from '../data/data';
 import { WorkoutData } from '../models/workout.model';
 
+import { WorkoutService } from './workout.service';
 
 describe('WorkoutService', () => {
   let service: WorkoutService;
@@ -10,36 +11,54 @@ describe('WorkoutService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(WorkoutService);
-    localStorage.clear(); // Ensure clean localStorage before testing.
   });
+
+  afterEach(() => {
+    localStorage.clear();
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
+
+  // Testing initializing data into localstorage
   it('should initialize workout data if not present in localStorage', () => {
-    service = new WorkoutService();
-    const storedData = JSON.parse(localStorage.getItem('workoutStorage')!);
+    const storedData = service.getStorageData();
+
     expect(storedData).toBeTruthy();
     expect(storedData.currentId).toBe(4);
     expect(Array.isArray(storedData?.workoutData)).toBeTrue();
   });
 
-  it('should add a new workout successfully', () => {
-    service = new WorkoutService();
 
-    const newWorkout: WorkoutData = {
-      name: 'Leg Day',
-      workouts: [{ type: 'Cycling', minutes: 45 }]
-    };
+  // Test helper method getStorageData
+  it('should return storedData if available in localstorage else return null', () => {
+    let storedData = service.getStorageData();
+    expect(storedData).toBeTruthy();
 
-    service.addWorkout(newWorkout);
-    const storedData = JSON.parse(localStorage.getItem('workoutStorage')!);
-    console.log(service,storedData?.workoutData)
-    expect(storedData.workoutData.length).toBe(4);
-    expect(storedData.workoutData[3].name).toBe('Leg Day');
+    localStorage.clear();
+
+    storedData = service.getStorageData();
+    expect(storedData).toBeNull()
   });
 
+  // Test helper method setStorageData
+  it('should set storedData in localstorage', () => {
+    localStorage.clear();
+
+    const data = {
+      currentId: 4,
+      workoutData: workoutData
+    };
+    service.setStorageData(data);
+
+    let storedData = service.getStorageData();
+    expect(storedData).toBeTruthy();
+  });
+
+
+  //Test missing or incomplete data
   it('should throw an error when adding a workout with incomplete data', () => {
     const incompleteWorkout: any = {
       name: 'Incomplete Workout',
@@ -49,7 +68,10 @@ describe('WorkoutService', () => {
     expect(() => service.addWorkout(incompleteWorkout)).toThrowError('Workout data is incomplete');
   });
 
+  //Test limit of 20 records
   it('should limit the number of stored workouts to 20', () => {
+    localStorage.clear();
+
     for (let i = 0; i < 20; i++) {
       service.addWorkout({ id: i, name: `Workout ${i}`, workouts: [{ type: 'Cycling', minutes: 45 }] });
     }
@@ -57,4 +79,77 @@ describe('WorkoutService', () => {
     expect(() => service.addWorkout({ id: 21, name: 'Overflow', workouts: [{ type: 'Cycling', minutes: 45 }] }))
       .toThrowError('Maximum of 20 records allowed.');
   });
+
+  //Test addition of new workout data
+  it('should add a new workout successfully', () => {
+
+    const newWorkout: WorkoutData = {
+      name: 'Leg Day',
+      workouts: [{ type: 'Cycling', minutes: 45 }]
+    };
+
+    service.addWorkout(newWorkout);
+    const storedData = service.getStorageData();
+
+    expect(storedData.workoutData.length).toBe(4);
+    expect(storedData.workoutData[3].name).toBe('Leg Day');
+  });
+
+
+  // Get all Workouts data tests
+  it('should return first 3 workouts by default', () => {
+
+    const result = service.getAllWorkouts();
+    expect(result?.length).toBe(3);
+  });
+
+  // filtering tests cases
+  it('should throw an error when no filters are provided', () => {
+    expect(() => service.filterWorkouts(null, null, 1)).toThrowError('At least one filter must be provided: workout type or search name.');
+  });
+
+  it('should not throw an error when at least one filter is provided', () => {
+    expect(() => service.filterWorkouts('cardio', null)).not.toThrow();
+  });
+
+  it('should return an empty array when no workouts match filter criteria', () => {
+
+    const result = service.filterWorkouts('Boxing', 'Nonexistent Name', 1);
+    expect(result).toEqual([]);
+  });
+
+  it('should filter by workout type and name correctly', () => {
+
+    const result = service.filterWorkouts('Cycling', 'John', 1);
+    expect(result?.length).toBe(2);
+    expect(result[0].name).toBe('John Doe');
+  });
+
+  it('should filter by workout type only', () => {
+
+    const result = service.filterWorkouts('Running', null, 1);
+    expect(result?.length).toBe(2);
+  });
+
+  it('should filter by name only', () => {
+
+    const result = service.filterWorkouts(null, 'Jane', 1);
+    expect(result?.length).toBe(1);
+    expect(result[0].name).toBe('Jane Smith');
+  });
+
+  it("should return 4 data records based on pagination on page 4", () => {
+    localStorage.clear();
+
+    for (let i = 1; i < 20; i++) {
+      service.addWorkout({ id: i, name: `Workout ${i}`, workouts: [{ type: 'Running', minutes: 45 }] });
+    }
+
+    const result = service.filterWorkouts('Running', null, 4);
+    expect(result?.length).toBe(4);
+    expect(result[3].name).toBe('Workout 19');
+
+  })
+
+
 });
--- src/app/services/workout.service.ts
+++ src/app/services/workout.service.ts
@@ -17,6 +17,7 @@ export class WorkoutService {
   // Initialize workout data and currentId in localStorage if not already set
   private initializeWorkouts() {
     const storedData = localStorage.getItem(this.storageKey);
+
     if (!storedData) {
       const initialData = {
         currentId: 4,
@@ -27,13 +28,13 @@ export class WorkoutService {
   }
 
   // Helper method to get data from localStorage
-  private getStorageData() {
+  public getStorageData() {
     const storedData = localStorage.getItem(this.storageKey);
     return storedData ? JSON.parse(storedData) : null;
   }
 
   // Helper method to set data in localStorage
-  private setStorageData(data: { currentId: number, workoutData: WorkoutData }) {
+  public setStorageData(data: { currentId: number, workoutData: WorkoutData[] }) {
     localStorage.setItem(this.storageKey, JSON.stringify(data));
   }
 
@@ -65,5 +66,49 @@ export class WorkoutService {
       currentId: currentId,
       workoutData: workouts
     });
+  }
+
+  // get all workouts
+  getAllWorkouts(page: number = 1) {
+    const pageSize = 5;
+    const startIndex = (page - 1) * pageSize;
+
+    const data = this.getStorageData()?.workoutData;
+
+    // Paginate the data - return only 5 records per page
+    const paginatedData = data?.slice(startIndex, startIndex + pageSize);
+
+    return paginatedData;
+  }
+
+  // filter data based on workoutType and searchName
+  filterWorkouts(workoutType: string | null, searchName: string | null, page: number = 1) {
+    // Check if both parameters are not provided
+    if (!workoutType && !searchName) {
+      throw new Error('At least one filter must be provided: workout type or search name.');
+    }
+
+    const data = this.getStorageData().workoutData;
+
+    // Filter the workout data based on the provided filters
+    const filteredData = data?.filter((eachWorkOut: WorkoutData) => {
+
+      const workoutMatches = workoutType ?
+        eachWorkOut.workouts.some(workout => workout.type.toLowerCase() === workoutType.toLowerCase())
+        : true;
+
+      const nameMatches = searchName ?
+        eachWorkOut.name.toLowerCase().includes(searchName.toLowerCase())
+        : true;
+
+      return workoutMatches && nameMatches;
+    });
+
+    // limit to 5 records per page
+    const pageSize = 5;
+    const startIndex = (page - 1) * pageSize;
+    const paginatedData = filteredData?.slice(startIndex, startIndex + pageSize);
+
+    return paginatedData;
   }
 }
