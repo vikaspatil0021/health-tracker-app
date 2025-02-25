0ba77e4d0b52e65a96541a0c601dddbddeeca93e
Vikas Patil <vikaspatil0021o+github@gmail.com>
2025-02-25 11:11:53 +0530

    setup tailwind and primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'health-tracker';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    
+    //primeNG config
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: 'none'
+        }
+      }
+    })]
 };
